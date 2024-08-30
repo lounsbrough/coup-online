6b60ca53b934bfeccbabec9ebfa922d1e1fce8ea
David Lounsbrough <david.lounsbrough@gmail.com>
2024-08-30 15:59:00 -0500

    starting to define game types

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import http from 'http';
 import express from 'express';
 import cors from 'cors';
+import { getGameState } from './utilities/gameState';
 
 const app = express();
 app.use(cors());
@@ -8,7 +9,35 @@ const server = http.createServer(app);
 
 const port = 8000;
 
-app.get('/publicGameState', function (req, res) {
+app.get('/gameState', function (req, res) {
+    res.json({});
+});
+
+app.post('/action', async (req, res) => {
+    const roomId = req.body.roomId;
+    const playerId = req.body.playerId;
+
+    if (!roomId || !playerId) {
+        res.status(400).send('room id and player id are required');
+        return;
+    }
+
+    const gameState = await getGameState(roomId);
+
+    if (!gameState) {
+        res.status(400).send('invalid room id');
+        return;
+    }
+
+    const player = gameState.players.find(({ id }) => id === playerId)
+
+    if (!player) {
+        res.status(400).send('player not in room');
+        return;
+    }
+
+    // try to process action
+
     res.json({});
 });
 
---
+++ server/types/game.ts
@@ -0,0 +1,116 @@
+export enum Actions {
+  Assassinate = 'Assassinate',
+  Steal = 'Steal',
+  Coup = 'Coup',
+  Tax = 'Tax',
+  ForeignAid = 'ForeignAid',
+  Income = 'Income',
+  Exchange = 'Exchange'
+}
+
+export const ActionRules: {
+  [action in Actions]: {
+    blockable: boolean
+    challengeable: boolean
+    coinsRequired?: number
+  }
+} = {
+  [Actions.Assassinate]: {
+    blockable: true,
+    challengeable: true,
+    coinsRequired: 3
+  },
+  [Actions.Steal]: {
+    blockable: true,
+    challengeable: true
+  },
+  [Actions.Coup]: {
+    blockable: false,
+    challengeable: false,
+    coinsRequired: 7
+  },
+  [Actions.Tax]: {
+    blockable: false,
+    challengeable: true
+  },
+  [Actions.ForeignAid]: {
+    blockable: true,
+    challengeable: false
+  },
+  [Actions.Income]: {
+    blockable: false,
+    challengeable: false
+  },
+  [Actions.Exchange]: {
+    blockable: false,
+    challengeable: true
+  }
+}
+
+export enum Influences {
+  Assassin = 'Assassin',
+  Contessa = 'Contessa',
+  Captain = 'Captain',
+  Ambassador = 'Ambassador',
+  Duke = 'Duke'
+}
+
+export const InfluencesRules: {
+  [influence in Influences]: {
+    legalAction?: Actions
+    legalBlock?: Actions
+  }
+} = {
+  [Influences.Assassin]: {
+    legalAction: Actions.Assassinate
+  },
+  [Influences.Contessa]: {
+    legalBlock: Actions.Assassinate
+  },
+  [Influences.Captain]: {
+    legalAction: Actions.Steal,
+    legalBlock: Actions.Steal
+  },
+  [Influences.Ambassador]: {
+    legalAction: Actions.Exchange,
+    legalBlock: Actions.Steal
+  },
+  [Influences.Duke]: {
+    legalAction: Actions.Tax,
+    legalBlock: Actions.ForeignAid
+  }
+}
+
+export type Player = {
+  influences: Influences[],
+  coins: number
+  name: string
+  id: string
+}
+
+export type PublicPlayer = Omit<Player, 'id' | 'influences'> & {
+  influenceCount: number
+};
+
+export type GameState = {
+  players: Player[]
+  turnPlayer: string
+  pendingAction: {
+    targetPlayer?: string
+    action: Actions
+  }
+  pendingActionChallenge?: {
+    sourcePlayer: string
+  }
+  pendingBlock?: {
+    sourcePlayer: string
+    claimedInfluence: Influences
+  }
+  pendingBlockChallenge?: {
+    sourcePlayer: string
+  }
+}
+
+export type PublicGameState = Omit<GameState, 'players'> & {
+  players: PublicPlayer[]
+}
