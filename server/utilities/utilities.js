04c5eca4f8c812185642959ecc4db33f31258789
David Lounsbrough <david.lounsbrough@gmail.com>
2024-08-30 14:44:04 -0500

    test ci again

--- server/utilities/utilities.js
+++ server/utilities/utilities.js
@@ -1,6 +1,6 @@
 function generateNamespace(length = 6) {
-   var result           = '';
-   var characters       = 'ABCDEFGHIJKLMNPQRSTUVWXYZ123456789';
+   var result = '';
+   var characters = 'ABCDEFGHIJKLMNPQRSTUVWXYZ123456789';
    var charactersLength = characters.length;
    for (var i = 0; i < length; i++) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
