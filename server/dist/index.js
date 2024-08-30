b6aa04acf58aa35184dde57af1352fd32bb8def4
David Lounsbrough <david.lounsbrough@gmail.com>
2024-08-30 16:08:27 -0500

    rebuild and fix redis issues

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,16 +1,32 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const http_1 = __importDefault(require("http"));
-const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
-const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
-const server = http_1.default.createServer(app);
+import http from 'http';
+import express from 'express';
+import cors from 'cors';
+import { getGameState } from './utilities/gameState';
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
 const port = 8000;
-app.get('/publicGameState', function (req, res) {
+app.get('/gameState', function (req, res) {
+    res.json({});
+});
+app.post('/action', async (req, res) => {
+    const roomId = req.body.roomId;
+    const playerId = req.body.playerId;
+    if (!roomId || !playerId) {
+        res.status(400).send('room id and player id are required');
+        return;
+    }
+    const gameState = await getGameState(roomId);
+    if (!gameState) {
+        res.status(400).send('invalid room id');
+        return;
+    }
+    const player = gameState.players.find(({ id }) => id === playerId);
+    if (!player) {
+        res.status(400).send('player not in room');
+        return;
+    }
+    // try to process action
     res.json({});
 });
 server.listen(process.env.PORT || port, function () {
--- server/utilities/gameState.ts
+++ server/utilities/gameState.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'redis';
 import { GameState } from '../types/game';
 
-const redisClient = await createClient(process.env.REDIS_CONNECTION_STRING)
+const redisClientPromise = createClient({ url: process.env.REDIS_CONNECTION_STRING })
   .on('error', (error: Error) => console.log('Redis Client Error', error))
   .connect();
 
@@ -11,12 +11,12 @@ const gameStates: {
 
 export const getGameState = async (roomId: string) => {
   if (!gameStates[roomId]) {
-    gameStates[roomId] = await redisClient.get(roomId);
+    gameStates[roomId] = JSON.parse(await (await redisClientPromise).get(roomId));
   }
   return gameStates[roomId];
 }
 
 export const setGameState = async (roomId: string, newState: GameState) => {
-  await redisClient.set(roomId, newState);
+  await (await redisClientPromise).set(roomId, JSON.stringify(newState));
   gameStates[roomId] = newState;
 }
