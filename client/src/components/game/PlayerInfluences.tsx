98e9a41097fc0966d5bc68efe38c0d3d173b2afb
David Lounsbrough <david.lounsbrough@gmail.com>
2024-09-02 10:30:56 -0500

    PlayerInfluences and Player components

--- client/src/components/game/PlayerInfluences.tsx
+++ client/src/components/game/PlayerInfluences.tsx
@@ -1,16 +1,30 @@
-import { Box, Grid2 } from "@mui/material";
-import { InfluenceAttributes, Player } from '../../../../shared/types/game'
+import { Box, Grid2, Typography } from "@mui/material";
+import { InfluenceAttributes, Player } from '../../shared/types/game'
+import { Circle } from "@mui/icons-material";
 
-function PlayerInfluences({ player }: { player: Player }) {
+function PlayerInfluences({ player } : { player: Player }) {
   return (
-    <Grid2 container>
-      {player.influences.map((influence, index) =>
-        <Grid2 key={index}>
-          <Box sx={{ background: InfluenceAttributes[influence].color }}>{influence}</Box>
-          {influence}
-        </Grid2>
-      )}
-    </Grid2>
+    <>
+      <Typography sx={{ mb: 1 }}>Your Influences</Typography>
+      <Grid2 container justifyContent="center">
+        {player.influences.map((influence, index) =>
+          <Grid2 key={index}>
+            <Box sx={{ px: 3 }}>
+              <Circle sx={{
+                fontSize: '2rem',
+                color: InfluenceAttributes[influence].color
+              }} />
+              <Typography sx={{
+                fontWeight: 'bold',
+                fontSize: '1.2rem'
+              }}
+              >{influence}
+              </Typography>
+            </Box>
+          </Grid2>
+        )}
+      </Grid2>
+    </>
   )
 }
 
