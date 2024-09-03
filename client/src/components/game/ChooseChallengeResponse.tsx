340da301588514616839fde8d1d14418a601e082
David Lounsbrough <david.lounsbrough@gmail.com>
2024-09-03 16:03:16 -0500

    more logic for challenge response

---
+++ client/src/components/game/ChooseChallengeResponse.tsx
@@ -0,0 +1,53 @@
+import { Button, Grid2, Tooltip, Typography } from "@mui/material";
+import { ActionAttributes, Actions, InfluenceAttributes, Influences, PublicGameState, ResponseAttributes, Responses } from "../../shared/types/game";
+import useSWRMutation from "swr/mutation";
+import { useState } from "react";
+import { getPlayerId } from "../../helpers/playerId";
+
+function ChooseChallengeResponse({ roomId, gameState }: { roomId: string, gameState: PublicGameState }) {
+  const [error, setError] = useState<string>();
+
+  const { trigger, isMutating, error: swrError } = useSWRMutation(`${process.env.REACT_API_BASE_URL ?? 'http://localhost:8000'}/challengeResponse`, (async (url: string, { arg }: { arg: { roomId: string, playerId: string; influence: Influences }; }) => {
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(arg)
+    }).then(async (res) => {
+      if (!res.ok) {
+        setError('Error responding to challenge');
+      }
+    })
+  }))
+
+  return (
+    <>
+      <Typography sx={{ my: 1, fontWeight: 'bold', fontSize: '24px' }}>
+        Choose an Influence to Reveal:
+      </Typography>
+      <Grid2 container spacing={2} justifyContent="center">
+        {gameState.selfPlayer.influences.map((influence, index) => {
+          return <Button
+            key={index}
+            onClick={() => {
+              trigger({
+                roomId,
+                playerId: getPlayerId(),
+                influence: influence as Influences
+              })
+            }}
+            sx={{
+              background: InfluenceAttributes[influence].color
+            }} variant="contained"
+          >
+            {influence}
+          </Button>
+        })}
+      </Grid2>
+      {error && <Typography sx={{ mt: 3, fontWeight: 700, color: 'red' }}>{error}</Typography>}
+    </>
+  );
+}
+
+export default ChooseChallengeResponse;
--- client/src/components/game/PlayerDecision.tsx
+++ client/src/components/game/PlayerDecision.tsx
@@ -2,6 +2,7 @@ import { Typography } from "@mui/material";
 import { PublicGameState } from "../../shared/types/game";
 import ChooseAction from "./ChooseAction";
 import ChooseActionResponse from "./ChooseActionResponse";
+import ChooseChallengeResponse from "./ChooseChallengeResponse";
 
 function PlayerDecision({ roomId, gameState }: {
   roomId: string, gameState: PublicGameState
@@ -24,6 +25,16 @@ function PlayerDecision({ roomId, gameState }: {
     !gameState.pendingActionChallenge &&
     gameState.pendingAction.pendingPlayers.includes(gameState.selfPlayer.name)) {
     return <ChooseActionResponse roomId={roomId} gameState={gameState} />;
+  }
+
+  if (turnPlayer?.name === gameState.selfPlayer.name &&
+    gameState.pendingActionChallenge) {
+    return <ChooseChallengeResponse roomId={roomId} gameState={gameState} />;
+  }
+
+  if (turnPlayer?.name === gameState.selfPlayer.name &&
+    gameState.pendingBlockChallenge) {
+    return <ChooseChallengeResponse roomId={roomId} gameState={gameState} />;
   }
 
   return <Typography>Waiting for other players</Typography>;
