fa17130a22f8602887bd92284047aa4a88a86d0a
David Lounsbrough <david.lounsbrough@gmail.com>
2024-09-03 20:28:00 -0500

    updating paths

--- client/src/components/game/InfluenceCard.tsx
+++ client/src/components/game/InfluenceCard.tsx
@@ -1,5 +1,5 @@
 import { Typography } from "@mui/material";
-import { Player } from '../../../../shared/types/game'
+import { Player } from "../../shared/types/game";
 
 function InfluenceCard({ player }: { player: Player }) {
   return <Typography>{JSON.stringify(player)}</Typography>
