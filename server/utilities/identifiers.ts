f1a410be20563f1d0e165a4d30ec071f7cc4063c
David Lounsbrough <david.lounsbrough@gmail.com>
2024-09-09 11:32:57 -0500

    first tests for server

--- server/utilities/identifiers.ts
+++ server/utilities/identifiers.ts
@@ -1,9 +1,9 @@
 export const generateRoomId = () => {
-   let result = '';
-   const characters = 'ABCDEFGHIJKLMNPQRSTUVWXYZ123456789';
-   const charactersLength = characters.length;
-   for (let i = 0; i < 6; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-   }
-   return result;
+   const characterCodes = [{ start: 48, count: 10 }, { start: 65, count: 26 }]
+      .flatMap(({ start, count }) =>
+         Array.from({ length: count }, (_, i) => start + i));
+
+   return Array.from({ length: 6 }, () =>
+      String.fromCharCode(characterCodes[Math.floor(Math.random() * characterCodes.length)])
+   ).join('');
 };
