a0313530e789aea9e863ec9e9aa3e6521bd30be2
David Lounsbrough <david.lounsbrough@gmail.com>
2024-09-11 15:20:11 -0500

    starting RTL tests

---
+++ client/src/components/utilities/ColoredTypography.spec.tsx
@@ -0,0 +1,45 @@
+import ColoredTypography from "./ColoredTypography"
+import { render, getRandomGameState, getCurrentColorMode } from '../../../tests/utilities/render'
+import { ActionAttributes, Actions, InfluenceAttributes, Influences } from "../../shared/types/game"
+
+describe('ColoredTypography', () => {
+  it('should render a single element when no matches', async () => {
+    const { findByText } = render(<ColoredTypography>text to color</ColoredTypography>)
+
+    await findByText('text to color')
+  })
+
+  it('should render a single element when no matches', async () => {
+    const gameState = getRandomGameState()
+
+    gameState.players[0].name = 'David'
+    gameState.players[1].name = 'Dаvid'
+
+    const { findByText } = render(
+      <>
+        <ColoredTypography>David is trying to use Coup on Dаvid</ColoredTypography>
+        <ColoredTypography>someone has lost their Contessa</ColoredTypography>
+      </>,
+      { gameState }
+    )
+
+    const colorMode = await getCurrentColorMode()
+
+    const expectedSegments = [
+      { text: 'David', color: gameState.players[0].color },
+      { text: ' is trying to use ' },
+      { text: 'Coup', color: ActionAttributes[Actions.Coup].color[colorMode] },
+      { text: ' on ' },
+      { text: 'Dаvid', color: gameState.players[1].color },
+      { text: 'someone has lost their ' },
+      { text: 'Contessa', color: InfluenceAttributes[Influences.Contessa].color[colorMode] }
+    ]
+
+    for (const expectedSegment of expectedSegments) {
+      const segment = await findByText(expectedSegment.text, { trim: false })
+      if (expectedSegment.color) {
+        expect(segment).toHaveStyle({ color: expectedSegment.color })
+      }
+    }
+  })
+})
---
+++ client/tests/utilities/render.tsx
@@ -0,0 +1,70 @@
+
+import React from "react"
+import { screen, render as testingLibraryRender } from "@testing-library/react"
+import Chance from 'chance'
+import { GameStateContext } from "../../src/context/GameStateContext"
+import { PublicGameState } from "../../src/shared/types/game"
+import { MaterialThemeContextProvider, useColorModeContext } from "../../src/context/MaterialThemeContext"
+import { PaletteMode } from "@mui/material"
+
+const chance = new Chance()
+
+export const getRandomGameState = (): PublicGameState => {
+  const players = chance.n(() => ({
+    name: chance.string(),
+    coins: chance.natural({ min: 0, max: 12 }),
+    influenceCount: chance.natural({ min: 0, max: 2 }),
+    color: chance.color()
+  }), chance.natural({ min: 2, max: 6 }))
+
+  const selfPlayer = chance.pickone(players)
+
+  return {
+    deadCards: [],
+    players: players,
+    roomId: chance.string(),
+    eventLogs: [],
+    isStarted: true,
+    pendingInfluenceLoss: {},
+    pendingAction: undefined,
+    pendingActionChallenge: undefined,
+    pendingBlock: undefined,
+    pendingBlockChallenge: undefined,
+    turnPlayer: chance.string(),
+    selfPlayer: {
+      coins: selfPlayer.coins,
+      color: selfPlayer.color,
+      id: chance.string(),
+      influences: [],
+      name: selfPlayer.name
+    }
+  }
+}
+
+export const render = (jsx: React.JSX.Element, {
+  gameState
+}: {
+  gameState?: PublicGameState
+} = {}) => {
+  const TestComponent = () => {
+    const { colorMode } = useColorModeContext()
+
+    return <MaterialThemeContextProvider>
+      <GameStateContext.Provider value={{
+        setGameState: () => { },
+        gameState: gameState ?? getRandomGameState()
+      }}>
+        <>
+          <span data-testid="current-color-mode">{colorMode}</span>
+          {jsx}
+        </>
+      </GameStateContext.Provider>
+    </MaterialThemeContextProvider>
+  }
+
+  return testingLibraryRender(<TestComponent />)
+}
+
+export const getCurrentColorMode = async () => {
+  return (await screen.findByTestId('current-color-mode')).textContent! as PaletteMode
+}
