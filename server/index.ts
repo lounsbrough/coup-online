a7a549d40e9fc6a559c4d91f4466a67ace93938b
David Lounsbrough <david.lounsbrough@gmail.com>
2024-09-14 15:16:33 -0500

    small refactor

--- server/index.ts
+++ server/index.ts
@@ -5,7 +5,7 @@ import cors from 'cors'
 import Joi, { ObjectSchema } from 'joi'
 import { Actions, Influences, Responses, PublicGameState } from '../shared/types/game'
 import { actionChallengeResponseHandler, actionHandler, actionResponseHandler, blockChallengeResponseHandler, blockResponseHandler, createGameHandler, getGameStateHandler, joinGameHandler, loseInfluenceHandler, resetGameHandler, startGameHandler } from './src/game/actionHandlers'
-import { GameMutationError } from './src/utilities/errors'
+import { GameMutationInputError } from './src/utilities/errors'
 
 const port = process.env.EXPRESS_PORT || 8008
 
@@ -13,10 +13,6 @@ const app = express()
 app.use(cors())
 app.use(json())
 const server = http.createServer(app)
-
-type PublicGameStateOrError = PublicGameState | {
-    error: string
-}
 
 const playerNameRule = Joi.string().min(1).max(10).disallow(
     ...Object.values(Influences),
@@ -38,12 +34,14 @@ const validateRequest = (schema: ObjectSchema, requestProperty: 'body' | 'query'
 const validateBody = (schema: ObjectSchema) => validateRequest(schema, 'body')
 const validateQuery = (schema: ObjectSchema) => validateRequest(schema, 'query')
 
-const processHandler = <T>(handler: (props: T) =>
-    Promise<PublicGameState>) => async (res: Response, props: T) => {
+type PublicGameStateOrError = PublicGameState | { error: string }
+
+const responseHandler = <T>(handler: (props: T) =>
+    Promise<PublicGameState>) => async (res: Response<PublicGameStateOrError>, props: T) => {
         try {
             res.status(200).json(await handler(props))
         } catch (error) {
-            if (error instanceof GameMutationError) {
+            if (error instanceof GameMutationInputError) {
                 res.status(error.httpCode).json({ error: error.message })
             } else {
                 throw error
@@ -54,68 +52,53 @@ const processHandler = <T>(handler: (props: T) =>
 app.get('/gameState', validateQuery(Joi.object().keys({
     roomId: Joi.string().required(),
     playerId: Joi.string().required()
-})), (async (
-    req: Request,
-    res: Response<PublicGameStateOrError>
-) => {
+})), (async (req, res) => {
     const roomId: string = req.query.roomId as string
     const playerId: string = req.query.playerId as string
 
-    return processHandler(getGameStateHandler)(res, { roomId, playerId })
+    await responseHandler(getGameStateHandler)(res, { roomId, playerId })
 }))
 
 app.post('/createGame', validateBody(Joi.object().keys({
     playerId: Joi.string().required(),
     playerName: playerNameRule
-})), async (
-    req: Request,
-    res: Response<PublicGameStateOrError>
-) => {
+})), async (req, res) => {
     const playerId: string = req.body.playerId
     const playerName: string = req.body.playerName
 
-    return processHandler(createGameHandler)(res, { playerId, playerName })
+    await responseHandler(createGameHandler)(res, { playerId, playerName })
 })
 
 app.post('/joinGame', validateBody(Joi.object().keys({
     roomId: Joi.string().required(),
     playerId: Joi.string().required(),
     playerName: playerNameRule
-})), async (
-    req: Request,
-    res: Response<PublicGameStateOrError>
-) => {
+})), async (req, res) => {
     const roomId: string = req.body.roomId
     const playerId: string = req.body.playerId
     const playerName: string = req.body.playerName.trim()
 
-    return processHandler(joinGameHandler)(res, { roomId, playerId, playerName })
+    await responseHandler(joinGameHandler)(res, { roomId, playerId, playerName })
 })
 
 app.post('/resetGame', validateBody(Joi.object().keys({
     roomId: Joi.string().required(),
     playerId: Joi.string().required()
-})), async (
-    req: Request,
-    res: Response<PublicGameStateOrError>
-) => {
+})), async (req, res) => {
     const roomId: string = req.body.roomId
     const playerId: string = req.body.playerId
 
-    return processHandler(resetGameHandler)(res, { roomId, playerId })
+    await responseHandler(resetGameHandler)(res, { roomId, playerId })
 })
 
 app.post('/startGame', validateBody(Joi.object().keys({
     roomId: Joi.string().required(),
     playerId: Joi.string().required()
-})), async (
-    req: Request,
-    res: Response<PublicGameStateOrError>
-) => {
+})), async (req, res) => {
     const roomId: string = req.body.roomId
     const playerId: string = req.body.playerId
 
-    return processHandler(startGameHandler)(res, { roomId, playerId })
+    await responseHandler(startGameHandler)(res, { roomId, playerId })
 })
 
 app.post('/action', validateBody(Joi.object().keys({
@@ -123,16 +106,13 @@ app.post('/action', validateBody(Joi.object().keys({
     playerId: Joi.string().required(),
     action: Joi.string().allow(...Object.values(Actions)).required(),
     targetPlayer: Joi.string()
-})), async (
-    req: Request,
-    res: Response<PublicGameStateOrError>
-) => {
+})), async (req, res) => {
     const roomId: string = req.body.roomId
     const playerId: string = req.body.playerId
     const action: Actions = req.body.action
     const targetPlayer: string | undefined = req.body.targetPlayer
 
-    return processHandler(actionHandler)(res, { roomId, playerId, action, targetPlayer })
+    await responseHandler(actionHandler)(res, { roomId, playerId, action, targetPlayer })
 })
 
 app.post('/actionResponse', validateBody(Joi.object().keys({
@@ -140,76 +120,61 @@ app.post('/actionResponse', validateBody(Joi.object().keys({
     playerId: Joi.string().required(),
     response: Joi.string().allow(...Object.values(Responses)).required(),
     claimedInfluence: Joi.string().allow(...Object.values(Influences))
-})), async (
-    req: Request,
-    res: Response<PublicGameStateOrError>
-) => {
+})), async (req, res) => {
     const roomId: string = req.body.roomId
     const playerId: string = req.body.playerId
     const response: Responses = req.body.response
     const claimedInfluence: Influences | undefined = req.body.claimedInfluence
 
-    return processHandler(actionResponseHandler)(res, { roomId, playerId, response, claimedInfluence })
+    await responseHandler(actionResponseHandler)(res, { roomId, playerId, response, claimedInfluence })
 })
 
 app.post('/actionChallengeResponse', validateBody(Joi.object().keys({
     roomId: Joi.string().required(),
     playerId: Joi.string().required(),
     influence: Joi.string().allow(...Object.values(Influences)).required()
-})), async (
-    req: Request,
-    res: Response<PublicGameStateOrError>
-) => {
+})), async (req, res) => {
     const roomId: string = req.body.roomId
     const playerId: string = req.body.playerId
     const influence: Influences = req.body.influence
 
-    return processHandler(actionChallengeResponseHandler)(res, { roomId, playerId, influence })
+    await responseHandler(actionChallengeResponseHandler)(res, { roomId, playerId, influence })
 })
 
 app.post('/blockResponse', validateBody(Joi.object().keys({
     roomId: Joi.string().required(),
     playerId: Joi.string().required(),
     response: Joi.string().allow(...Object.values(Responses)).required()
-})), async (
-    req: Request,
-    res: Response<PublicGameStateOrError>
-) => {
+})), async (req, res) => {
     const roomId: string = req.body.roomId
     const playerId: string = req.body.playerId
     const response: Responses = req.body.response
 
-    return processHandler(blockResponseHandler)(res, { roomId, playerId, response })
+    await responseHandler(blockResponseHandler)(res, { roomId, playerId, response })
 })
 
 app.post('/blockChallengeResponse', validateBody(Joi.object().keys({
     roomId: Joi.string().required(),
     playerId: Joi.string().required(),
     influence: Joi.string().allow(...Object.values(Influences)).required()
-})), async (
-    req: Request,
-    res: Response<PublicGameStateOrError>
-) => {
+})), async (req, res) => {
     const roomId: string = req.body.roomId
     const playerId: string = req.body.playerId
     const influence: Influences = req.body.influence
 
-    return processHandler(blockChallengeResponseHandler)(res, { roomId, playerId, influence })
+    await responseHandler(blockChallengeResponseHandler)(res, { roomId, playerId, influence })
 })
 
 app.post('/loseInfluence', validateBody(Joi.object().keys({
     roomId: Joi.string().required(),
     playerId: Joi.string().required(),
     influence: Joi.string().allow(...Object.values(Influences)).required()
-})), async (
-    req: Request,
-    res: Response<PublicGameStateOrError>
-) => {
+})), async (req, res) => {
     const roomId: string = req.body.roomId
     const playerId: string = req.body.playerId
     const influence: Influences = req.body.influence
 
-    return processHandler(loseInfluenceHandler)(res, { roomId, playerId, influence })
+    await responseHandler(loseInfluenceHandler)(res, { roomId, playerId, influence })
 })
 
 server.listen(port, function () {
