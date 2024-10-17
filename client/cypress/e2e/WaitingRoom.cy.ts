2486f8575f2e29cb52675b62b2ec63d60cfc5d02
David Lounsbrough <david.lounsbrough@gmail.com>
2024-10-16 22:41:19 -0500

    back to passing

--- client/cypress/e2e/WaitingRoom.cy.ts
+++ client/cypress/e2e/WaitingRoom.cy.ts
@@ -1,6 +1,5 @@
 describe('Waiting Room', () => {
   it('should allow players to create, join, leave, and start games', () => {
     cy.visit('/')
-    cy.contains('thing that does not exist')
   })
 })
