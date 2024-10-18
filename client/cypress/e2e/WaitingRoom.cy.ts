2b9ce43260f7b46549dcaa605a9c7a73e2fc653c
David Lounsbrough <david.lounsbrough@gmail.com>
2024-10-17 22:50:27 -0500

    test removing players from games

--- client/cypress/e2e/WaitingRoom.cy.ts
+++ client/cypress/e2e/WaitingRoom.cy.ts
@@ -29,6 +29,24 @@ describe('Waiting Room', () => {
 
     cy.contains('button', 'Join Game').click()
 
+    cy.get('*[data-testid="CloseIcon"').eq(0).click()
+
+    cy.contains('Add at least one more player to start game')
+
+    cy.visit('/', {
+      onBeforeLoad({ localStorage }) {
+        localStorage.setItem(playerIdStorageKey, 'player1')
+      }
+    })
+    cy.contains('button', 'Join Existing Game').click()
+
+    cy.getCookie('cypressRoomId').then((cookie) => {
+      cy.get('input').eq(0).should('not.be.disabled').type(cookie!.value)
+      cy.get('input').eq(1).should('not.be.disabled').type('Player One')
+    })
+
+    cy.contains('button', 'Join Game').click()
+
     cy.contains('button', 'Start Game').click()
   })
 })
