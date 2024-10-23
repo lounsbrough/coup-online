15543dc8e2846c59109b4492b93b654b16439dcc
David Lounsbrough <david.lounsbrough@gmail.com>
2024-10-22 22:06:58 -0500

    start logic for choosing action

--- server/src/game/ai.spec.ts
+++ server/src/game/ai.spec.ts
@@ -1,20 +1,20 @@
 import { Chance } from 'chance'
-import { Influences, PublicGameState, PublicPlayer } from '../../../shared/types/game'
-import { getProbabilityOfPlayerInfluence } from './ai'
+import { Actions, Influences, PublicGameState, PublicPlayer } from '../../../shared/types/game'
+import { decideAction, getPlayerDangerFactor, getProbabilityOfPlayerInfluence } from './ai'
 
 const chance = new Chance()
 
 describe('ai', () => {
-  describe('getProbabilityOfPlayerInfluence', () => {
-    const getRandomPlayer = (): PublicPlayer => ({
-      name: chance.string(),
-      coins: chance.natural(),
-      influenceCount: 2,
-      deadInfluences: [],
-      color: chance.string(),
-      ai: chance.bool()
-    })
+  const getRandomPlayer = (): PublicPlayer => ({
+    name: chance.string(),
+    coins: chance.natural(),
+    influenceCount: 2,
+    deadInfluences: [],
+    color: chance.string(),
+    ai: chance.bool()
+  })
 
+  describe('getProbabilityOfPlayerInfluence', () => {
     const testCases: {
       testCase: string
       gameState: PublicGameState,
@@ -25,7 +25,7 @@ describe('ai', () => {
           testCase: '2 hidden cards',
           gameState: {
             roomId: chance.string(),
-            isStarted: true,
+            isStarted: chance.bool(),
             eventLogs: [],
             players: [
               {
@@ -40,8 +40,8 @@ describe('ai', () => {
               }
             ],
             selfPlayer: {
-              id: chance.string(),
               ...getRandomPlayer(),
+              id: chance.string(),
               influences: [Influences.Ambassador],
               deadInfluences: [Influences.Contessa]
             },
@@ -55,7 +55,7 @@ describe('ai', () => {
           testCase: '2 cards revealed and self holding last card',
           gameState: {
             roomId: chance.string(),
-            isStarted: true,
+            isStarted: chance.bool(),
             eventLogs: [],
             players: [
               {
@@ -75,8 +75,8 @@ describe('ai', () => {
               }
             ],
             selfPlayer: {
-              id: chance.string(),
               ...getRandomPlayer(),
+              id: chance.string(),
               influences: [Influences.Assassin],
               deadInfluences: [Influences.Ambassador]
             },
@@ -90,7 +90,7 @@ describe('ai', () => {
           testCase: 'self holding 2 cards and last card hidden',
           gameState: {
             roomId: chance.string(),
-            isStarted: true,
+            isStarted: chance.bool(),
             eventLogs: [],
             players: [
               {
@@ -105,8 +105,8 @@ describe('ai', () => {
               },
             ],
             selfPlayer: {
-              id: chance.string(),
               ...getRandomPlayer(),
+              id: chance.string(),
               influences: [Influences.Duke, Influences.Duke],
               deadInfluences: []
             },
@@ -120,7 +120,7 @@ describe('ai', () => {
           testCase: 'all cards revealed for influence',
           gameState: {
             roomId: chance.string(),
-            isStarted: true,
+            isStarted: chance.bool(),
             eventLogs: [],
             players: [
               {
@@ -140,8 +140,8 @@ describe('ai', () => {
               }
             ],
             selfPlayer: {
-              id: chance.string(),
               ...getRandomPlayer(),
+              id: chance.string(),
               influences: [Influences.Ambassador],
               deadInfluences: [Influences.Assassin]
             },
@@ -164,6 +164,136 @@ describe('ai', () => {
       influence: Influences
     }) => {
       expect(getProbabilityOfPlayerInfluence(gameState, influence)).toBeCloseTo(probability)
+    })
+  })
+
+  describe('getPlayerDangerFactor', () => {
+    const testCases: {
+      testCase: string
+      player: PublicPlayer
+      dangerFactor: number
+    }[] = [
+        {
+          testCase: 'dead player with some coins',
+          player: {
+            ...getRandomPlayer(),
+            influenceCount: 0,
+            coins: 12
+          },
+          dangerFactor: 0
+        },
+        {
+          testCase: '1 influence left with 0 coins',
+          player: {
+            ...getRandomPlayer(),
+            influenceCount: 1,
+            coins: 0
+          },
+          dangerFactor: 10
+        },
+        {
+          testCase: '1 influence left with 12 coins',
+          player: {
+            ...getRandomPlayer(),
+            influenceCount: 1,
+            coins: 12
+          },
+          dangerFactor: 22
+        },
+        {
+          testCase: '2 influences left with 0 coins',
+          player: {
+            ...getRandomPlayer(),
+            influenceCount: 2,
+            coins: 0
+          },
+          dangerFactor: 20
+        },
+        {
+          testCase: '2 influences left with 12 coins',
+          player: {
+            ...getRandomPlayer(),
+            influenceCount: 2,
+            coins: 12
+          },
+          dangerFactor: 32
+        }
+      ]
+
+    it.each(testCases)('should return $dangerFactor for $testCase', ({ player, dangerFactor }) => {
+      expect(getPlayerDangerFactor(player)).toBeCloseTo(dangerFactor)
+    })
+  })
+
+  describe('decideAction', () => {
+    it('should choose Coup if 10 or more coins', () => {
+      expect(decideAction({
+        roomId: chance.string(),
+        isStarted: chance.bool(),
+        eventLogs: [],
+        players: [
+          {
+            ...getRandomPlayer(),
+            name: 'harper',
+            influenceCount: 2,
+            deadInfluences: []
+          },
+          {
+            ...getRandomPlayer(),
+            name: 'david',
+            influenceCount: 2,
+            deadInfluences: []
+          }
+        ],
+        selfPlayer: {
+          ...getRandomPlayer(),
+          id: chance.string(),
+          name: 'harper',
+          coins: 10,
+          influences: [Influences.Ambassador, Influences.Contessa],
+          deadInfluences: []
+        },
+        pendingInfluenceLoss: {},
+        deckCount: 11
+      })).toEqual({
+        action: Actions.Coup,
+        targetPlayer: 'david'
+      })
+    })
+
+    it('should choose Coup if 7 or more coins and checkmate', () => {
+      expect(decideAction({
+        roomId: chance.string(),
+        isStarted: chance.bool(),
+        eventLogs: [],
+        players: [
+          {
+            ...getRandomPlayer(),
+            name: 'harper',
+            influenceCount: 1,
+            deadInfluences: [Influences.Contessa]
+          },
+          {
+            ...getRandomPlayer(),
+            name: 'david',
+            influenceCount: 1,
+            deadInfluences: [Influences.Captain]
+          }
+        ],
+        selfPlayer: {
+          ...getRandomPlayer(),
+          id: chance.string(),
+          name: 'harper',
+          coins: 7,
+          influences: [Influences.Ambassador],
+          deadInfluences: [Influences.Contessa]
+        },
+        pendingInfluenceLoss: {},
+        deckCount: 11
+      })).toEqual({
+        action: Actions.Coup,
+        targetPlayer: 'david'
+      })
     })
   })
 })
--- server/src/game/ai.ts
+++ server/src/game/ai.ts
@@ -1,5 +1,5 @@
 import { countOfEachInfluenceInDeck } from "../utilities/gameState"
-import { Influences, PublicGameState } from "../../../shared/types/game"
+import { Actions, Influences, PublicGameState, PublicPlayer } from "../../../shared/types/game"
 
 export const getProbabilityOfPlayerInfluence = (
   gameState: PublicGameState,
@@ -22,4 +22,47 @@ export const getProbabilityOfPlayerInfluence = (
     gameState.deckCount
 
   return (countOfEachInfluenceInDeck - knownMatchedInfluenceCount) / (totalInfluenceCount - knownInfluences.length)
+}
+
+export const getPlayerDangerFactor = (player: PublicPlayer) => {
+  if (!player.influenceCount) {
+    return 0
+  }
+
+  const dangerFactor = player.influenceCount * 10 + player.coins
+
+  return dangerFactor
+}
+
+export const decideAction = (gameState: PublicGameState): {
+  action: Actions
+  targetPlayer?: string
+} => {
+  if (!gameState.selfPlayer) {
+    throw new Error('AI could not determine self player')
+  }
+
+  const opponents = gameState.players.filter(({ influenceCount, name }) =>
+    influenceCount && name !== gameState.selfPlayer?.name)
+
+  let willCoup = false
+  if (gameState.selfPlayer?.coins >= 10) {
+    willCoup = true
+  } else if (gameState.selfPlayer?.coins >= 7) {
+    if (opponents.length === 1 && opponents[0].influenceCount === 1) {
+      willCoup = true
+    } else {
+      willCoup = Math.random() > 0.5
+    }
+  }
+
+  if (willCoup) {
+    const targetPlayer = opponents.sort((a, b) =>
+      getPlayerDangerFactor(b) * Math.random() * 0.05 -
+      getPlayerDangerFactor(a) * Math.random() * 0.05)[0]
+
+    return { action: Actions.Coup, targetPlayer: targetPlayer.name }
+  }
+
+  return { action: Actions.Income }
 }
