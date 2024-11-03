57dfaaf5cae0efd7431a5d042dd0962e084405eb
David Lounsbrough <david.lounsbrough@gmail.com>
2024-11-02 22:03:37 -0500

    even more likely to steal

--- server/src/game/ai.ts
+++ server/src/game/ai.ts
@@ -92,7 +92,7 @@ export const decideAction = (gameState: PublicGameState): {
   }
 
   if (gameState.selfPlayer.influences.includes(Influences.Captain) || Math.random() > 0.95) {
-    if (getProbabilityOfPlayerInfluence(gameState, Influences.Captain) < 0.2 + Math.random() * 0.2) {
+    if (getProbabilityOfPlayerInfluence(gameState, Influences.Captain) < 0.4 + Math.random() * 0.4) {
       const targetPlayer = getTargetPlayer(gameState)
       return { action: Actions.Steal, targetPlayer: targetPlayer.name }
       // TODO: check for soft target, needs to track in state
