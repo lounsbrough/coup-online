3dff2dd167eb15d4ccc866ded66d7aa9a68e8fd5
David Lounsbrough <david.lounsbrough@gmail.com>
2025-01-06 16:16:11 -0600

    moving to react-router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './components/App'
 import reportWebVitals from './reportWebVitals'
--- client/src/setupTests.ts
+++ client/src/setupTests.ts
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+import { TextEncoder, TextDecoder } from 'util'
+
+// https://github.com/jsdom/jsdom/issues/2524
+global.TextEncoder = TextEncoder
+global.TextDecoder = TextDecoder
