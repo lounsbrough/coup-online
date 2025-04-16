a943e26185abb34bd77b054c69877cc33415a2b2
David Lounsbrough <david.lounsbrough@gmail.com>
2025-04-15 22:07:16 -0500

    remove dead rules code

--- client/src/components/rules/RulesDrawerContent.tsx
+++ client/src/components/rules/RulesDrawerContent.tsx
@@ -1,6 +1,6 @@
 import { useMemo } from "react"
 import { Box, DialogContent, DialogContentText, Divider, Typography, useTheme } from "@mui/material"
-import { Block, Group } from "@mui/icons-material"
+import { Group } from "@mui/icons-material"
 import { ActionAttributes, Actions, Influences } from '@shared'
 import InfluenceIcon from "../icons/InfluenceIcon"
 import { useTranslationContext } from "../../contexts/TranslationsContext"
@@ -9,8 +9,6 @@ import './Rules.css'
 export default function RulesDrawerContent() {
   const { breakpoints, actionColors, influenceColors } = useTheme()
   const { t } = useTranslationContext()
-
-  const isSmallScreen = true
 
   const influenceText = useMemo(() => Object.fromEntries(
     Object.values(Influences).map((influence) =>
@@ -23,12 +21,11 @@ export default function RulesDrawerContent() {
     )
   ), [t])
 
-  const noneIndicator = <><Block sx={{ verticalAlign: 'middle', }} />{isSmallScreen ? <br /> : null}<span style={{ verticalAlign: 'middle' }}>{` ${t('none')}`}</span></>
-  const anyIndicator = <><Group sx={{ verticalAlign: 'middle', }} />{isSmallScreen ? <br /> : null}<span style={{ verticalAlign: 'middle' }}>{` ${t('anyone')}`}</span></>
+  const anyIndicator = <><Group sx={{ verticalAlign: 'middle', }} /><br /><span style={{ verticalAlign: 'middle' }}>{` ${t('anyone')}`}</span></>
 
   return (
     <DialogContent sx={{
-      px: isSmallScreen ? 4 : undefined,
+      px: 4,
       [breakpoints.up('md')]: { px: undefined },
       textAlign: 'center'
     }}>
@@ -38,191 +35,102 @@ export default function RulesDrawerContent() {
           sx={{ fontWeight: "bold" }}
         >{t('cheatSheet')}</Typography>
         <Box sx={{ mt: 3 }}>
-          {isSmallScreen
-            ? (
-              <table className="cheat-sheet-table small-screen">
-                <tbody>
-                  <tr style={{ background: actionColors[Actions.Income] }}>
-                    <td>{anyIndicator}</td>
-                    <td>
-                      {t(Actions.Income)}
-                      <br />
-                      {t('collectCoins', { count: 1 })}
-                    </td>
-                  </tr>
-                  <tr style={{ background: actionColors[Actions.ForeignAid] }}>
-                    <td>{anyIndicator}</td>
-                    <td>
-                      {t(Actions.ForeignAid)}
-                      <br />
-                      {t('collectCoins', { count: 2 })}
-                    </td>
-                  </tr>
-                  <tr style={{ background: actionColors[Actions.Coup] }}>
-                    <td>{anyIndicator}</td>
-                    <td>
-                      {t(Actions.Coup)}
-                      <br />
-                      {t('payCoins', { count: 7 })}
-                      <br />
-                      {t('killAnInfluence')}
-                    </td>
-                  </tr>
-                  <tr style={{ background: influenceColors[Influences.Duke] }}>
-                    <td>
-                      <InfluenceIcon influence={Influences.Duke} />
-                      <br />
-                      {t(Influences.Duke)}
-                    </td>
-                    <td>
-                      {t(Actions.Tax)}
-                      <br />
-                      {t('collectCoins', { count: 3 })}
-                      <br />
-                      {t('block')} {t(Actions.ForeignAid)}
-                    </td>
-                  </tr>
-                  <tr style={{ background: influenceColors[Influences.Assassin] }}>
-                    <td>
-                      <InfluenceIcon influence={Influences.Assassin} />
-                      <br />
-                      {t(Influences.Assassin)}
-                    </td>
-                    <td>
-                      {t(Actions.Assassinate)}
-                      <br />
-                      {t('payCoins', { count: 3 })}
-                      <br />
-                      {t('killAnInfluence')}
-                    </td>
-                  </tr>
-                  <tr style={{ background: influenceColors[Influences.Ambassador] }}>
-                    <td>
-                      <InfluenceIcon influence={Influences.Ambassador} />
-                      <br />
-                      {t(Influences.Ambassador)}
-                    </td>
-                    <td>
-                      {t(Actions.Exchange)}
-                      <br />
-                      {t('draw2InfluencesAndDiscard2')}
-                      <br />
-                      {t('block')} {t(Actions.Steal)}
-                    </td>
-                  </tr>
-                  <tr style={{ background: influenceColors[Influences.Captain] }}>
-                    <td>
-                      <InfluenceIcon influence={Influences.Captain} />
-                      <br />
-                      {t(Influences.Captain)}
-                    </td>
-                    <td>
-                      {t(Actions.Steal)}
-                      <br />
-                      {t('steal2CoinsFromSomeone')}
-                      <br />
-                      {t('block')} {t(Actions.Steal)}
-                    </td>
-                  </tr>
-                  <tr style={{ background: influenceColors[Influences.Contessa] }}>
-                    <td>
-                      <InfluenceIcon influence={Influences.Contessa} />
-                      <br />
-                      {t(Influences.Contessa)}
-                    </td>
-                    <td>
-                      {t('block')} {t(Actions.Assassinate)}
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            ) : (
-              <table className="cheat-sheet-table large-screen">
-                <thead>
-                  <tr>
-                    <th>{t('influence')}</th>
-                    <th>{t('action')}</th>
-                    <th>{t('effect')}</th>
-                    <th>{t('block')}</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr style={{ background: actionColors[Actions.Income] }}>
-                    <td>{anyIndicator}</td>
-                    <td>{t(Actions.Income)}</td>
-                    <td>{t('collectCoins', { count: 1 })}</td>
-                    <td></td>
-                  </tr>
-                  <tr style={{ background: actionColors[Actions.ForeignAid] }}>
-                    <td>{anyIndicator}</td>
-                    <td>{t(Actions.ForeignAid)}</td>
-                    <td>{t('collectCoins', { count: 2 })}</td>
-                    <td></td>
-                  </tr>
-                  <tr style={{ background: actionColors[Actions.Coup] }}>
-                    <td>{anyIndicator}</td>
-                    <td>{t(Actions.Coup)}</td>
-                    <td>{t('payCoins', { count: 7 })} - {t('killAnInfluence')}</td>
-                    <td></td>
-                  </tr>
-                  <tr style={{
-                    background: influenceColors[Influences.Duke]
-                  }}>
-                    <td>
-                      <InfluenceIcon influence={Influences.Duke} />
-                      <span> {t(Influences.Duke)}</span>
-                    </td>
-                    <td>{t(Actions.Tax)}</td>
-                    <td>{t('collectCoins', { count: 3 })}</td>
-                    <td>{t(Actions.ForeignAid)}</td>
-                  </tr>
-                  <tr style={{
-                    background: influenceColors[Influences.Assassin]
-                  }}>
-                    <td>
-                      <InfluenceIcon influence={Influences.Assassin} />
-                      <span> {t(Influences.Assassin)}</span>
-                    </td>
-                    <td>{t(Actions.Assassinate)}</td>
-                    <td>{t('payCoins', { count: 3 })} - {t('killAnInfluence')}</td>
-                    <td>{noneIndicator}</td>
-                  </tr>
-                  <tr style={{
-                    background: influenceColors[Influences.Ambassador]
-                  }}>
-                    <td>
-                      <InfluenceIcon influence={Influences.Ambassador} />
-                      <span> {t(Influences.Ambassador)}</span>
-                    </td>
-                    <td>{t(Actions.Exchange)}</td>
-                    <td>{t('draw2InfluencesAndDiscard2')}</td>
-                    <td>{t(Actions.Steal)}</td>
-                  </tr>
-                  <tr style={{
-                    background: influenceColors[Influences.Captain]
-                  }}>
-                    <td>
-                      <InfluenceIcon influence={Influences.Captain} />
-                      <span> {t(Influences.Captain)}</span>
-                    </td>
-                    <td>{t(Actions.Steal)}</td>
-                    <td>{t('steal2CoinsFromSomeone')}</td>
-                    <td>{t(Actions.Steal)}</td>
-                  </tr>
-                  <tr style={{
-                    background: influenceColors[Influences.Contessa]
-                  }}>
-                    <td>
-                      <InfluenceIcon influence={Influences.Contessa} />
-                      <span> {t(Influences.Contessa)}</span>
-                    </td>
-                    <td>{noneIndicator}</td>
-                    <td>{noneIndicator}</td>
-                    <td>{t(Actions.Assassinate)}</td>
-                  </tr>
-                </tbody>
-              </table>
-            )}
+          <table className="cheat-sheet-table">
+            <tbody>
+              <tr style={{ background: actionColors[Actions.Income] }}>
+                <td>{anyIndicator}</td>
+                <td>
+                  {t(Actions.Income)}
+                  <br />
+                  {t('collectCoins', { count: 1 })}
+                </td>
+              </tr>
+              <tr style={{ background: actionColors[Actions.ForeignAid] }}>
+                <td>{anyIndicator}</td>
+                <td>
+                  {t(Actions.ForeignAid)}
+                  <br />
+                  {t('collectCoins', { count: 2 })}
+                </td>
+              </tr>
+              <tr style={{ background: actionColors[Actions.Coup] }}>
+                <td>{anyIndicator}</td>
+                <td>
+                  {t(Actions.Coup)}
+                  <br />
+                  {t('payCoins', { count: 7 })}
+                  <br />
+                  {t('killAnInfluence')}
+                </td>
+              </tr>
+              <tr style={{ background: influenceColors[Influences.Duke] }}>
+                <td>
+                  <InfluenceIcon influence={Influences.Duke} />
+                  <br />
+                  {t(Influences.Duke)}
+                </td>
+                <td>
+                  {t(Actions.Tax)}
+                  <br />
+                  {t('collectCoins', { count: 3 })}
+                  <br />
+                  {t('block')} {t(Actions.ForeignAid)}
+                </td>
+              </tr>
+              <tr style={{ background: influenceColors[Influences.Assassin] }}>
+                <td>
+                  <InfluenceIcon influence={Influences.Assassin} />
+                  <br />
+                  {t(Influences.Assassin)}
+                </td>
+                <td>
+                  {t(Actions.Assassinate)}
+                  <br />
+                  {t('payCoins', { count: 3 })}
+                  <br />
+                  {t('killAnInfluence')}
+                </td>
+              </tr>
+              <tr style={{ background: influenceColors[Influences.Ambassador] }}>
+                <td>
+                  <InfluenceIcon influence={Influences.Ambassador} />
+                  <br />
+                  {t(Influences.Ambassador)}
+                </td>
+                <td>
+                  {t(Actions.Exchange)}
+                  <br />
+                  {t('draw2InfluencesAndDiscard2')}
+                  <br />
+                  {t('block')} {t(Actions.Steal)}
+                </td>
+              </tr>
+              <tr style={{ background: influenceColors[Influences.Captain] }}>
+                <td>
+                  <InfluenceIcon influence={Influences.Captain} />
+                  <br />
+                  {t(Influences.Captain)}
+                </td>
+                <td>
+                  {t(Actions.Steal)}
+                  <br />
+                  {t('steal2CoinsFromSomeone')}
+                  <br />
+                  {t('block')} {t(Actions.Steal)}
+                </td>
+              </tr>
+              <tr style={{ background: influenceColors[Influences.Contessa] }}>
+                <td>
+                  <InfluenceIcon influence={Influences.Contessa} />
+                  <br />
+                  {t(Influences.Contessa)}
+                </td>
+                <td>
+                  {t('block')} {t(Actions.Assassinate)}
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </Box>
         <Divider sx={{ my: 8 }} />
         <Typography
