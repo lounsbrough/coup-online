57a1e23e23e5f25acf423058d5bb7e35fa09873c
David Lounsbrough <david.lounsbrough@gmail.com>
2025-05-02 22:39:06 -0500

    block forfeit on client side when more than 2 influences

--- client/src/components/game/Forfeit.tsx
+++ client/src/components/game/Forfeit.tsx
@@ -5,31 +5,45 @@ import { getPlayerId } from "../../helpers/players"
 import useGameMutation from "../../hooks/useGameMutation"
 import { useGameStateContext } from "../../contexts/GameStateContext"
 import { useTranslationContext } from "../../contexts/TranslationsContext"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const ForfeitIcon = Flag
 
 function Forfeit() {
-  const [confimrationOpen, setConfirmationOpen] = useState(false)
+  const [confirmationOpen, setConfirmationOpen] = useState(false)
+  const [error, setError] = useState('')
 
   const forfeitMutation = useGameMutation<{
     roomId: string, playerId: string
-  }>({ action: PlayerActions.forfeit })
+  }>({
+    action: PlayerActions.forfeit, callback: () => {
+      setConfirmationOpen(false)
+    }
+  })
 
   const { gameState } = useGameStateContext()
   const { t } = useTranslationContext()
 
-  if (!gameState) {
+  useEffect(() => {
+    if (!confirmationOpen) setError('')
+  }, [confirmationOpen])
+
+  useEffect(() => {
+    if (forfeitMutation.error) setError(forfeitMutation.error)
+  }, [forfeitMutation.error])
+
+  if (!gameState?.selfPlayer) {
     return null
   }
 
-  const selfPlayerName = gameState.selfPlayer?.name
-  const playerIsDead = !gameState.selfPlayer?.influences.length
+  const selfPlayerName = gameState.selfPlayer.name
+  const playerIsDead = !gameState.selfPlayer.influences.length
   const playerHasActionsPending =
     (gameState.turnPlayer === selfPlayerName && !!gameState.pendingAction)
     || gameState.pendingActionChallenge?.sourcePlayer === selfPlayerName
     || gameState.pendingBlock?.sourcePlayer === selfPlayerName
     || gameState.pendingBlockChallenge?.sourcePlayer === selfPlayerName
+    || gameState.selfPlayer.influences.length > 2
 
   return (
     <>
@@ -54,7 +68,7 @@ function Forfeit() {
         )}
       </Box>
       <Dialog
-        open={confimrationOpen}
+        open={confirmationOpen}
         onClose={() => { setConfirmationOpen(false) }}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
@@ -65,7 +79,7 @@ function Forfeit() {
         <DialogContent>
           <DialogContentText>
             {t('forfeitConfirmationMessage')}
-            {forfeitMutation.error && <Typography color='error' sx={{ mt: 3, fontWeight: 700 }}>{forfeitMutation.error}</Typography>}
+            {error && <Typography color='error' sx={{ mt: 3, fontWeight: 700 }}>{error}</Typography>}
           </DialogContentText>
         </DialogContent>
         <DialogActions>
@@ -84,7 +98,6 @@ function Forfeit() {
                 roomId: gameState.roomId,
                 playerId: getPlayerId()
               })
-              setConfirmationOpen(false)
             }}
             disabled={playerHasActionsPending}
             loading={forfeitMutation.isMutating}
