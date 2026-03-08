1cc653c703a439156ce100201993e9eec67f6099
David Lounsbrough <david.lounsbrough@gmail.com>
2026-03-07 22:10:03 -0600

    these are now links

--- client/cypress/e2e/WaitingRoom.cy.ts
+++ client/cypress/e2e/WaitingRoom.cy.ts
@@ -1,7 +1,7 @@
 describe('Waiting Room', () => {
   it('should allow players to create, join, leave, and start games', () => {
     cy.loadPlayer('/', 'player1')
-    cy.contains('button', 'Create New Game').click()
+    cy.contains('a', 'Create New Game').click()
     cy.get('[data-testid="playerNameInput"]').should('not.be.disabled').type('Player 1')
     cy.contains('button', 'Create Game').click()
 
@@ -10,7 +10,7 @@ describe('Waiting Room', () => {
     })
 
     cy.loadPlayer('/', 'player2')
-    cy.contains('button', 'Join Existing Game').click()
+    cy.contains('a', 'Join Existing Game').click()
 
     cy.getCookie('cypressRoomId').then((cookie) => {
       cy.get('[data-testid="roomIdInput"]').should('not.be.disabled').type(cookie!.value)
@@ -24,7 +24,7 @@ describe('Waiting Room', () => {
     cy.contains('Add at least one more player to start game')
 
     cy.loadPlayer('/', 'player1')
-    cy.contains('button', 'Join Existing Game').click()
+    cy.contains('a', 'Join Existing Game').click()
 
     cy.getCookie('cypressRoomId').then((cookie) => {
       cy.get('[data-testid="roomIdInput"]').should('not.be.disabled').type(cookie!.value)
